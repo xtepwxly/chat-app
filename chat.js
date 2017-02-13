1dc9c7385048a4943bc6dc6f53f2c8b3642cdc20
Nnutthowze <briareibismuth@gmail.com>
2017-02-14 00:47:00 +0200

    Basic working chat with messed CSS

--- chat.js
+++ chat.js
@@ -7,12 +7,21 @@ const port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/index.html')));
 
-io.on('connection', (socket) => {
-    console.log('random user connected');
+io.on('connection', socket => {
+    socket.on('message_send', msg => {
+        io.emit('message_send', msg);
+    });
 });
 
 server.listen(port);
+
+
+// TODO:
+// 1. Broadcast a message to connected users when someone connects or disconnects
+// 2. Add support for nicknames
+// 3. Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
+// 4. Add “{user} is typing” functionality
+// 5. Show who’s online
+// 6. Add private messaging
